34e2e68674af572b7387e901bac66525184a758a
Himakar Reddy <himakar.galipuri@mobigesture.com>
2019-04-25 18:54:03 +0530

    cors,helmet,compression added. Routing errors handled centrally

--- server.js
+++ server.js
@@ -2,22 +2,32 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const listEndpoints = require('express-list-endpoints')
+const cors = require('cors')
+const helmet = require('helmet')
+const compression = require('compression')
 const config = require('./config')
 const app = express()
 
 const _listRoutes = () => {
     console.log(' ')
-            console.log('Available routes..')
-            console.log(listEndpoints(app))
-            console.log(' ')
+    console.log('Available routes..')
+    console.log(listEndpoints(app))
+    console.log(' ')
 }
 const start = () => {
     return new Promise((Resolve, Reject) => {
-        
+
 
         app.use(morgan('dev'))
         app.use(bodyParser.urlencoded({ extended: true }))
         app.use(bodyParser.json())
+        app.use(cors())
+        app.use(helmet())
+        app.use(compression())
+        app.use((err, req, res, next) => {
+            console.error(err.stack)
+            res.status(500).send('Something broke!')
+        })
 
         const routes = require('./routes/main')
 
@@ -25,7 +35,7 @@ const start = () => {
         console.log('Starting server..')
         app.listen(config.port, () => Resolve())
             .on('error', (err) => { Reject(err) })
-            _listRoutes()
+        _listRoutes()
     })
 
 }
