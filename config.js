d4166e07740488abcaf4e633fbab180b797d4a88
Himakar Reddy <himakar.galipuri@mobigesture.com>
2019-04-25 19:11:49 +0530

    config file removed from git. logs added through winston

--- config.js
+++
@@ -1,5 +0,0 @@
-// do not commit this file to fit. DB details will be exposed.
-module.exports = {
-    port: process.env.port || 3000,
-    dburl: process.env.dburl || "mongodb://test:2PhZsUpAZYzh6iB@ds221155.mlab.com:21155/mobigesture-testdb"
-}
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const listEndpoints = require('express-list-endpoints')
 const cors = require('cors')
 const helmet = require('helmet')
 const compression = require('compression')
+const winston = require('winston')
 const config = require('./config')
 const app = express()
 
@@ -14,11 +15,38 @@ const _listRoutes = () => {
     console.log(listEndpoints(app))
     console.log(' ')
 }
+
+let logger = winston.createLogger({
+    transports: [
+        new winston.transports.File({
+            level: 'info',
+            filename: './logs/all-logs.log',
+            handleExceptions: true,
+            json: true,
+            maxsize: 5242880, //5MB
+            maxFiles: 5,
+            colorize: false
+        }),
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            colorize: true
+        })
+    ],
+    exitOnError: false
+})
+
+logger.stream = {
+    write: function(message, encoding){
+        logger.info(message);
+    }
+};
 const start = () => {
     return new Promise((Resolve, Reject) => {
 
 
-        app.use(morgan('dev'))
+        app.use(morgan("combined", { "stream": logger.stream }))
         app.use(bodyParser.urlencoded({ extended: true }))
         app.use(bodyParser.json())
         app.use(cors())
