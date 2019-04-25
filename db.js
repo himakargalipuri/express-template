4a58876ce38ff87cc9bc0dd434a4c59bb1be70fe
Himakar Reddy <himakar.galipuri@mobigesture.com>
2019-04-25 18:10:10 +0530

    db file removed unwanted code

--- db.js
+++ db.js
@@ -2,12 +2,6 @@ var mongoose = require('mongoose');
 // Build the connection string 
 const dbURI = require('./config').dburl;
 
-mongoose.connect(dbURI, {useNewUrlParser: true});
-
-
-
-module.exports = mongoose;
-
 const connect = () => {
     console.log('Database connecting....')
     return mongoose.connect(dbURI, {useNewUrlParser: true})
